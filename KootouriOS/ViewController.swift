90b3923a1a67baac89d0edce4bebdbaef2a7ae1f
AyeMin <ayemin@kootour.com>
2017-10-13 15:34:33 -0700

    Initial Commit

---
+++ KootouriOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KootouriOS
+//
+//  Created by AyeMin on 2017-10-13.
+//  Copyright © 2017 Kootour. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
